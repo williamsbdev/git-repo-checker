d25e68ea5bb8ff14558785c3c4ea24a90875c9f1
Brandon Williams <williams.brandon86@gmail.com>
2012-11-19 07:58:59 -0600

    exporting status

--- statusOfGit.sh
+++ statusOfGit.sh
@@ -12,15 +12,20 @@ for dir in `find -name '.git' | sed 's/.git//g'`; do
 	    cd
 	    cd $dir
 	    git checkout $branch
-	    git status | grep 'nothing to commit' | wc >changes
-	    echo $changes
-	    if [ $changes -gt 0 ]; then
+#	    git status | grep 'nothing to commit' | wc
+	    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
+	    if [[ $STATUS != "" ]]
+#	    if [ $changes -gt 0 ]; then
 		echo $dir ' on branch ' $branch ' has changes to be committed'
 	    fi
 	done
-#    else
-#	echo "in else..."
-#	cd $dir
+    else
+	echo "in else..."
+	cd $dir
+	export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
+	if [[ $STATUS != "" ]]
+	    echo $dir ' on branch ' $branch ' has changes to be committed'
+	fi
 #	git status | grep 'nothing to commit' | wc >changes
 #       echo $changes
 #        if $changes > 0
