d974c61af2a03179621d5ce95e59b995aa5ea94c
Thomas <toran.billups@theimtgroup.com>
2012-11-19 10:18:14 -0600

    making some updates to check for correct message

--- statusOfGit.sh
+++ statusOfGit.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 #start from the home directory
 cd
-for dir in `find -name '.git' | sed 's/.git//g'`; do
+for dir in `find -P ~/Projects/imaging-service/ -name '.git' | sed 's/.git//g'`; do
     echo $dir
-    declare -a branches
-    if [ -d $dir.git/refs ]; then
-        echo "in if..."
-	cd $dir.git/refs/heads
-	for branch in `ls`; do
-	    echo $branch
-	    cd
-	    cd $dir
-	    git checkout $branch
-#	    git status | grep 'nothing to commit' | wc
-	    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
-	    if [[ $STATUS != "" ]]
-#	    if [ $changes -gt 0 ]; then
-		echo $dir ' on branch ' $branch ' has changes to be committed'
-	    fi
-	done
+    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
+    if [[ $STATUS != "" ]]; then
+        echo $dir ' on branch ' $branch ' has changes to be committed'
     else
-	echo "in else..."
-	cd $dir
-	export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
-	if [[ $STATUS != "" ]]
-	    echo $dir ' on branch ' $branch ' has changes to be committed'
-	fi
+        if [ -d $dir.git/refs ]; then
+            echo "in if..."
+            cd $dir.git/refs/heads
+            for branch in `ls`; do
+                echo $branch
+                cd
+                cd $dir
+                git checkout $branch
+            echo `git status`
+    	    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
+	        if [[ $STATUS != "" ]]; then
+#            if [ $changes -gt 0 ]; then
+                echo $dir ' on branch ' $branch ' has changes to be committed'
+            fi
+        done
+#    else
+#        echo "in else..."
+#    	cd $dir
+#	    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
+#    	if [[ $STATUS != "" ]]
+#            echo $dir ' on branch ' $branch ' has changes to be committed'
+#    	fi
 #	git status | grep 'nothing to commit' | wc >changes
 #       echo $changes
 #        if $changes > 0
