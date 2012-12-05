28e973f984b9c5335fe9d5b8a5d76462093bbeed
williamsb <williams.brandon86@gmail.com>
2012-12-04 21:49:55 -0600

    fixed the program to be usable

--- statusOfGit.sh
+++ statusOfGit.sh
@@ -1,40 +1,17 @@
 #!/bin/bash
 #start from the home directory
 cd
-for dir in `find -P ~/Projects/imaging-service/ -name '.git' | sed 's/.git//g'`; do
-    echo $dir
-    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
-    if [[ $STATUS != "" ]]; then
-        echo $dir ' on branch ' $branch ' has changes to be committed'
-    else
-        if [ -d $dir.git/refs ]; then
-            echo "in if..."
-            cd $dir.git/refs/heads
-            for branch in `ls`; do
-                echo $branch
-                cd
-                cd $dir
-                git checkout $branch
-            echo `git status`
-    	    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
-	        if [[ $STATUS != "" ]]; then
-#            if [ $changes -gt 0 ]; then
-                echo $dir ' on branch ' $branch ' has changes to be committed'
-            fi
-        done
-#    else
-#        echo "in else..."
-#    	cd $dir
-#	    export STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
-#    	if [[ $STATUS != "" ]]
-#            echo $dir ' on branch ' $branch ' has changes to be committed'
-#    	fi
-#	git status | grep 'nothing to commit' | wc >changes
-#       echo $changes
-#        if $changes > 0
-#	    echo $dir ' on branch ' $branch ' has changes to be committed'
-#        fi
+for dir in `find -name '.git' | sed 's/\(^.*\).git$/\1/'`; do
+    if [ -d $dir ]; then
+        cd $dir
+        GIT_STATUS=`git status | grep "Changes not staged for commit\|but untracked files present"`
+        if [ "$GIT_STATUS" != "" ]; then
+            branch=`git branch`
+            echo "$dir" 'on branch '
+            echo "$branch"
+            echo 'has changes to be committed'
+        fi
+        #return to home directory before working on next git repo
     fi
-    #return to home directory before working on next git repo
     cd
 done
