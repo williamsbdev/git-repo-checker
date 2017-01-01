3f5b157a77469a6fe3e764d85367ec690a501aa8
Brandon Williams <williamsbdev@gmail.com>
2016-12-31 20:39:21 -0600

    [ENHANCEMENT] update repo-checker to have cleaner output be more reusable

--- statusOfGit.sh
+++ statusOfGit.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
+function check_if_repo_is_clean() {
+    MESSAGE_TO_CHECK="$1"
+    GIT_STATUS=`git status | grep "${MESSAGE_TO_CHECK}"`
+    if [ "$GIT_STATUS" != "" ]; then
+        GIT_BRANCH=`git branch | grep "*" | awk -F ' ' '{print $2}'`
+        echo "${dir} on branch ${GIT_BRANCH} ${MESSAGE_TO_CHECK}"
+    fi
+}
+
 #start from the home directory
 cd
 echo "starting search for git repos..."
-for dir in `find ~ -name '.git' | sed 's/\(^.*\).git$/\1/'`; do
+for dir in `find ~ -name '.git' | sed 's/\(^.*\).git$/\1/' | grep -v "Library" | grep -v "dotfiles\/vim"`; do
     if [ -d $dir ]; then
-        cd $dir
-        branch=`git branch`
-        GIT_STATUS=`git status | grep "Changes not staged for commit"`
-        if [ "$GIT_STATUS" != "" ]; then
-            echo "$dir" 'on branch '
-            echo "$branch"
-            echo 'changes not staged for commit'
-        fi
-        GIT_STATUS=`git status | grep "but untracked files present"`
-        if [ "$GIT_STATUS" != "" ]; then
-            echo "$dir" 'on branch '
-            echo "$branch"
-            echo 'has untracked files'
-        fi
-        GIT_STATUS=`git status | grep "Your branch is ahead of"`
-        if [ "$GIT_STATUS" != "" ]; then
-            echo "$dir" 'on branch '
-            echo "$branch"
-            echo 'branch is ahead of origin'
-        fi
+        pushd $dir &>/dev/null
+        check_if_repo_is_clean "Changes not staged for commit"
+        check_if_repo_is_clean "but untracked files present"
+        check_if_repo_is_clean "Your branch is ahead of"
     fi
     #return to home directory before working on next git repo
-    cd
+    popd &>/dev/null
 done
